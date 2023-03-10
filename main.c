3edcdb6935008397154ec1c7a30fec82be867522
bksteele01 <bksteele01@gmail.com>
2023-03-10 11:44:24 -0500

    refined parser, and cd and pwd command recognized

--- main.c
+++ main.c
@@ -3,7 +3,12 @@
 #include <string.h>
 #include <unistd.h>
 
+enum cmds {CD=0, PWD=1};
+char *cmdstr[] = {"cd", "pwd"};
+
 void split_up2(char* command, char** params, int* paramnum);
+int execute(char** params, int paramnum);
+
 int main(){
     char command[100];
     char* params[7];
@@ -12,12 +17,10 @@ int main(){
         paramnum = 0;
         char* user = getenv("USER");
         printf("%s@shell>", user);
-        fgets(command, sizeof(command), stdin);      
+        fgets(command, sizeof(command), stdin);   
+        if(command[strlen(command)-1] == '\n') command[strlen(command)-1] = '\0';   
         split_up2(command, params, &paramnum);
-        for(int i=0;i<paramnum;i++){
-            printf("%s\n", params[i]);
-        }
-        printf("%d\n", paramnum);
+        execute(params, paramnum);
 
     }
 }
@@ -29,6 +32,24 @@ void split_up2(char* command, char** params, int* paramnum){
             break;
         }
         (*paramnum)++;
+    }
+}
+
+int execute(char** params, int paramnum){
+    int ncmds = sizeof(cmdstr) / sizeof(char *);
+    int i;
+    for(i=0;i<ncmds;i++){
+        if(strcmp(params[0], cmdstr[i]) == 0){
+            break;
+        }
+    }
+    switch(i){
+        case CD:
+            printf("you selected cd\n");
+            break;
+        case PWD:
+            printf("you selected pwd\n");
+            break;
     }
 }
 
